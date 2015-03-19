f235b0ab14fe045761a8022657aade16edfa1306
Roneesh Vashisht <rvashis@searshc.com>
2015-03-19 14:42:31 -0500

    first commit with Tasks scaffold created

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times { Task.create(description: (0..8).map{ ('a'..'z').to_a[rand(12)] }.join) }
